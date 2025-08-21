145b1db91ec73b81801fcd90fd65453507737072
zoevonallmen <z.vonallmen@gmail.com>
2025-08-21 14:20:18 +0200

    Added query with correct sources & dates

--- 01_Scripts/Swissdox.R
+++ 01_Scripts/Swissdox.R
@@ -12,3 +12,71 @@ headers <- add_headers(
 )
 
 API_URL_QUERY <- "https://swissdox.linguistik.uzh.ch/api/query"
+
+yaml_query <- "
+query:
+  sources:
+    - BLI
+    - SRF
+    - NZZ
+    - TA
+    - ZWA
+    - LUZ
+    - AZ
+    - BZ
+    - BU
+    - THT
+    - SGT
+  dates:
+    - from: 2013-10-09
+      to: 2014-02-09
+  languages:
+    - de
+  content:
+    AND:
+      - OR:
+              - einwander*
+              - zuwander*
+              - asyl*
+              - flüchtling*
+              - Personenfreizügigkeit
+              - überfremdung
+              - migrationspolitik
+              - migrationsabkommen
+              - abschiebung
+              - integrationspolitik
+result:
+  format: TSV
+  maxResults: 500000
+  columns:
+    - id
+    - pubtime
+    - medium_code
+    - medium_name
+    - rubric
+    - regional
+    - doctype
+    - doctype_description
+    - language
+    - char_count
+    - dateline
+    - head
+    - subhead
+    - content_id
+    - content
+version: 1.2
+"
+
+response <- POST(
+  url = API_URL_QUERY,
+  headers,
+  body = list(
+    query = yaml_query,
+    name = "Immigration Unlimited", ##change name next time!!
+    comment = "Query comment",
+    expirationDate = "2025-12-28"
+  ),
+  encode = "form"
+)
+
+print(content(response, "parsed"))
