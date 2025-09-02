b3f0b1bd13715d0f0af746363f07dbf833191a26
zoevonallmen <z.vonallmen@gmail.com>
2025-09-02 11:27:48 +0200

    random samplin 20 artciles/prompt testing

---
+++ 01_Scripts/Prompt Testing/Random Sample Articles.R
@@ -0,0 +1,16 @@
+
+# Load Data ---------------------------------------------------------------
+
+library(readr)
+articles <- read.csv("../Seminarpaper-Immigration-Media-Framing/Data/Final_Articles.csv")
+
+
+# Random Smaple -----------------------------------------------------------
+
+set.seed(2409)
+sampled_articles <- articles[sample(nrow(articles), 20), ]
+
+
+# Save as CSV -------------------------------------------------------------
+
+write_csv(sampled_articles, "../Seminarpaper-Immigration-Media-Framing/Data/Test_Articles_20.csv")
