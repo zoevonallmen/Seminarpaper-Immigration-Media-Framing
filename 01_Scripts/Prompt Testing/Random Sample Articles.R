4f3491bc67c1d0fa8f6f9366c0aab460d79a48db
zoevonallmen <z.vonallmen@gmail.com>
2025-09-03 16:21:50 +0200

    added sampling 50 articles randomly (for final prompt)

--- 01_Scripts/Prompt Testing/Random Sample Articles.R
+++ 01_Scripts/Prompt Testing/Random Sample Articles.R
@@ -14,3 +14,14 @@ sampled_articles <- articles[sample(nrow(articles), 20), ]
 # Save as CSV -------------------------------------------------------------
 
 write_csv(sampled_articles, "../Seminarpaper-Immigration-Media-Framing/Data/Test_Articles_20.csv")
+
+
+# Sampling 50 Artilces ---------------------------------------------------------
+
+articles <- read.csv("../Seminarpaper-Immigration-Media-Framing/Data/Final_Articles.csv")
+
+set.seed(2409)
+sampled_articles <- articles[sample(nrow(articles), 50), ]
+
+
+write_csv(sampled_articles, "../Seminarpaper-Immigration-Media-Framing/Data/Test_Articles_50.csv")
